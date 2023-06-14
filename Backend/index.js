c09cc4aa0798e004a7b0eebdf11c0058c4c4e93c
Amit Kumar Saini <107475338+iamamitkumarsaini@users.noreply.github.com>
2023-06-14 18:34:49 +0530

    Documenting index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,22 +7,32 @@ require("dotenv").config();
 
 const app = express();
 
+// Enable Cross-Origin Resource Sharing (CORS) for all routes.
+// Allow requests from any origin.
 app.use(
   cors({
     origin: "*",
   })
 );
 
+// Parsing JSON request bodies.
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to ProsesWebTechnologies_Assignment" });
 });
 
-
+// Middleware: Data Validation
+// Validate the data for "/user/add" and "/user/edit/:id" routes.
+// Ensures that the mobile number and email address are valid.
 app.use(dataValidation)
 app.use("/user", userRoutes);
 
+
+
+// Start the server and listen on the specified port.
+// Establishing a connection to the database.
+// Also logging the status of the database connection.
 app.listen(process.env.port, async (req, res) => {
   try {
     await connection;
