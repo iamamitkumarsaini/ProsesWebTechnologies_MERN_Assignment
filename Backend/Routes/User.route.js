feb32192abcc43fb1c021bca9b4466be710dd327
Amit Kumar Saini <107475338+iamamitkumarsaini@users.noreply.github.com>
2023-06-14 18:23:50 +0530

    Documenting Route

--- Backend/Routes/User.route.js
+++ Backend/Routes/User.route.js
@@ -3,6 +3,22 @@ const { UserModel } = require("../Models/User.model");
 
 const userRoutes = express.Router();
 
+//  Creates a new user profile.
+// POST  /user/add
+// Request Body:
+// username (string): The username for the new user.
+// mobileNum (number): The mobile number for the new user.
+// email (string): The email address for the new user.
+// address (string): The address for the new user.
+
+// Response:
+// If the username is available, the response will contain:
+// - message (string): "User Profile has been created"
+// - data (object): The created user profile.
+
+// If the username is already taken, the response will contain:
+// - message (string): "This Username is already taken"
+
 userRoutes.post("/add", async (req, res) => {
   const { username, mobileNum, email, address } = req.body;
 
@@ -25,6 +41,13 @@ userRoutes.post("/add", async (req, res) => {
   }
 });
 
+
+// Sending All user Data to client side
+// Retrieve all user profiles.
+// GET  /user
+// Response:
+// - If successful, the response will contain an array of user profiles.
+
 userRoutes.get("/", async (req, res) => {
   try {
     const userData = await UserModel.find();
@@ -34,6 +57,28 @@ userRoutes.get("/", async (req, res) => {
     res.send({ message: "Internal server error" });
   }
 });
+
+
+//  Updating an existing user profile.
+// PUT  /user/edit/:id
+
+// Request Parameters:
+// - id (string): The ID of the user to be updated.
+
+// Request Body:
+//  - username (string): The updated username for the user.
+//  - mobileNum (number): The updated mobile number for the user.
+//  - email (string): The updated email address for the user.
+//  - address (string): The updated address for the user.
+ 
+//  Response:
+//  - If successful, the response will contain:
+//  - message (string): "User Details Updated successfully"
+//  - data (object): The updated user profile.
+//  - If the user is not found, the response will contain:
+//  - message (string): "User not found"
+//  - If the requested username is already taken, the response will contain:
+//  - message (string): "Username is not available"
 
 userRoutes.put("/edit/:id", async (req, res) => {
   const payload = req.body;
@@ -71,6 +116,18 @@ userRoutes.put("/edit/:id", async (req, res) => {
   }
 });
 
+
+
+//  Deleting user profile.
+// DELETE  /user/delete/:id
+
+// Request Parameters:
+// - id (string): The ID of the user to be deleted.
+ 
+//  Response:
+//  - If successful, the response will contain:
+//  - message (string): "User deleted Successfully"
+
 userRoutes.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
 
@@ -82,6 +139,16 @@ userRoutes.delete("/delete/:id", async (req, res) => {
     res.send({ message: "Internal server error" });
   }
 });
+
+
+
+// Sending Single user Data to client side
+// GET  /user/:id
+
+// Request Parameters
+//  id (string): The ID of the user whom data to be send.
+// Response:
+// - If successful, the response will contain an array having a single element.
 
 userRoutes.get("/:id", async (req, res) => {
   const { id } = req.params;
@@ -95,4 +162,6 @@ userRoutes.get("/:id", async (req, res) => {
   }
 });
 
+
+// exporting userRoutes
 module.exports = { userRoutes };
